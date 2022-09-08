e7dd6f7c52969f134aee4a14b51f9cfca312d9b9
x0chaoren0s <x0chaoren0s@gmail.com>
2022-09-08 16:34:30 +0800

    多项改动，已完成 ①画轨迹+分割

--- deep_sort_app_my.py
+++ deep_sort_app_my.py
@@ -13,9 +13,11 @@ from deep_sort import nn_matching
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 
-from my_deep_sort.utils.data import Dataset
+from my_deep_sort.utils.data import LingshuiFrameDataset
 from my_deep_sort.utils.detector import Detector
-from my_deep_sort.utils.encoder import Encoder
+from my_deep_sort.utils.encoder import ApparentFeatureExtracker, ApparentFeatureCopier
+
+import random
 
 
 # 观测手段：图像识别检测器
@@ -24,7 +26,8 @@ checkpoint_file = '/home/xxy/mmdetection/work_dirs/mask2former_swin-s-p4-w7-224_
 detector = Detector(config_file, checkpoint_file)
 
 # 卡尔曼滤波更新辅助工具：表观特征抽取器
-encoder = Encoder('', '') # 还没实现，只写了接口extract_feature，因此其返回一个128维随机特征
+# apExtracker = ApparentFeatureExtracker('') # 还没实现，只写了接口extract_feature，因此其返回一个128维随机特征
+apExtracker = ApparentFeatureCopier('') # 直接循环复制 resources/detections/MOT16_POI_test/MOT16-06.npy 中的特征
 
 
 def create_detections(img, min_height=0):
@@ -32,8 +35,7 @@ def create_detections(img, min_height=0):
 
     Parameters
     ----------
-    img : str
-        The frame image file.
+    img : img 为单张图片的路径，或np.ndarray
     min_height : Optional[int]
         A minimum detection bounding box height. Detections that are smaller
         than this value are disregarded.
@@ -45,9 +47,10 @@ def create_detections(img, min_height=0):
 
     """
     
-    detections = detector.detect(img) #( [np.array(100个检测框：[x1,y1,w,h,confidence],[],..)],  [[100个mask：np.array(..),np.array()，..]]  )
-    detections = detections[0][0] # np.array(100个检测框：[x1,y1,w,h,confidence],[],..)
-    detections = [ (*d,*encoder.extract_feature(img, d)) for d in detections] # [ (tlwh,confidence,feature),(),.. ]
+    detections  = detector.detect(img) #( [np.array(100个检测框：[x1,y1,w,h,confidence],[],..)],  [[100个mask：np.array(..),np.array()，..]]  )
+    masks       = detections[1][0] # [100个mask：np.array(..),np.array()，..]
+    detections  = detections[0][0] # np.array(100个检测框：[x1,y1,w,h,confidence],[],..)
+    detections  = [ (*d,*apExtracker.extract_feature(img, d)) for d in detections] # [ (tlwh,confidence,feature),(),.. ]
 
     detection_list = []
     for d in detections: # row.shape: (138)
@@ -55,58 +58,43 @@ def create_detections(img, min_height=0):
         if bbox[3] < min_height:
             continue
         detection_list.append(Detection(bbox, confidence, feature)) # feature.shape:(128)
-    return detection_list # [ (tlwh,confidence,feature),(),.. ]
+    return detection_list, masks # [ (tlwh,confidence,feature),(),.. ], [100个mask：np.array(..),np.array()，..]
 
+def get_center(tlwh):
+    x,y,w,h = tlwh
+    return int(x+w/2), int(y+h/2)
 
-def run(sequence_dir, detection_file, output_file, min_confidence,
+def run(output_file, build_video, min_confidence,
         nms_max_overlap, min_detection_height, max_cosine_distance,
-        nn_budget, display):
-    """Run multi-target tracker on a particular sequence.
+        nn_budget, draw_masks, draw_detections, draw_tracks, draw_trails):
 
-    Parameters
-    ----------
-    sequence_dir : str
-        Path to the MOTChallenge sequence directory.
-    detection_file : str
-        Path to the detections file.
-    output_file : str
-        Path to the tracking output file. This file will contain the tracking
-        results on completion.
-    min_confidence : float
-        Detection confidence threshold. Disregard all detections that have
-        a confidence lower than this value.
-    nms_max_overlap: float
-        Maximum detection overlap (non-maxima suppression threshold).
-    min_detection_height : int
-        Detection height threshold. Disregard all detections that have
-        a height lower than this value.
-    max_cosine_distance : float
-        Gating threshold for cosine distance metric (object appearance).
-    nn_budget : Optional[int]
-        Maximum size of the appearance descriptor gallery. If None, no budget
-        is enforced.
-    display : bool
-        If True, show visualization of intermediate tracking results.
-
-    """
     metric = nn_matching.NearestNeighborDistanceMetric(
         "cosine", max_cosine_distance, nn_budget)
     tracker = Tracker(metric)
-    results = []    # [ [frame_idx, track_id, *tlwh],[],.. ]
+    results = []            # [ [frame_idx, track_id, *tlwh],[],.. ]
+    trails  = dict()        # { track_id:[[本track最近trail_len帧框的中心点],最近的frame_id] }
+    trail_len = 10          # 每条轨迹记录的最多历史帧数
+    track_colors = dict()   # { track_id: (r,g,b) } 用于统一条轨迹和其框的颜色
 
     # Run tracker.
     # 要推理的图片集合
-    dataset = Dataset()
+    dataset = LingshuiFrameDataset()
     def frame_callback(vis, frame_idx):
         print("Processing frame %05d" % frame_idx)
 
         # Load image and generate detections.
         img = dataset[frame_idx]
-        detections = create_detections(
+        detections, masks = create_detections(
             img, min_detection_height)
-        # detections: [ (tlwh,confidence,feature),(),.. ]
-        detections = [d for d in detections if d.confidence >= min_confidence]
-        # detections: [ (tlwh,confidence,feature),(),.. ]
+        # detections: [ (tlwh,confidence,feature),(),.. ], masks: [100个mask：np.array(..),np.array()，..]
+        # detections = [d for d in detections if d.confidence >= min_confidence]
+        tmp_detections, tmp_masks = [], []
+        for i,d in enumerate(detections):
+            if d.confidence >= min_confidence:
+                tmp_detections.append(d)
+                tmp_masks.append(masks[i])
+        detections, masks = tmp_detections, tmp_masks
+        # detections: [ (tlwh,confidence,feature),(),.. ], masks: [np.array(..),np.array()，..]
 
         # Run non-maxima suppression.
         boxes = np.array([d.tlwh for d in detections])
@@ -114,18 +102,39 @@ def run(sequence_dir, detection_file, output_file, min_confidence,
         indices = preprocessing.non_max_suppression(
             boxes, nms_max_overlap, scores)
         detections = [detections[i] for i in indices]
+        masks = [masks[i] for i in indices]
 
-        # Update tracker.
+        # Update tracker. 
         tracker.predict()
-        tracker.update(detections)  # detections: [ (tlwh,confidence,feature),(),.. ]
+        det_id_2_track_id = tracker.update(detections)  # detections: [ (tlwh,confidence,feature),(),.. ]
+            # det_id_2_track_id 第一部分来自于matches，第二部分来自于unmatched_detections
+            # det_id_2_track_id 用于产生标记展示图象的labels
+        labels = [str(det_id_2_track_id[det_id]) for det_id in range(len(detections))]
+        for det_idx in range(len(detections)):
+            track_idx = det_id_2_track_id[det_idx]
+            if track_idx not in track_colors:
+                track_colors[track_idx] = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        det_colors = [track_colors[det_id_2_track_id[det_id]] for det_id in range(len(detections))]
+
+        for detection_idx in det_id_2_track_id: # trails: { track_id:[[本track最近trail_len帧框的中心点],最近的frame_id] }
+            track_idx = det_id_2_track_id[detection_idx]
+            if track_idx not in trails:
+                trails[track_idx] = [[detections[detection_idx].get_center()], frame_idx]
+            else:
+                trails[track_idx][0].append(detections[detection_idx].get_center())
+                trails[track_idx][1] = frame_idx
+            trails[track_idx][0] = trails[track_idx][0][-trail_len:]
 
         # Update visualization.
-        if display:
-            image = cv2.imread(
-                img, cv2.IMREAD_COLOR)
-            vis.set_image(image.copy())
-            vis.draw_detections(detections)
-            vis.draw_trackers(tracker.tracks)
+        vis.set_image(img.copy())
+        if draw_detections:
+            vis.draw_detections(detections, labels, colors=det_colors)    # detections: [ (tlwh,confidence,feature),(),.. ]
+        if draw_tracks:
+            vis.draw_trackers(tracker.tracks, track_colors=track_colors)  
+        if draw_masks:
+            vis.draw_detection_masks(masks, colors=det_colors, labels=labels)
+        if draw_trails:
+            vis.draw_trails(trails, frame_idx, colors=det_colors)
 
         # Store results.
         for track in tracker.tracks:
@@ -136,9 +145,13 @@ def run(sequence_dir, detection_file, output_file, min_confidence,
                 frame_idx, track.track_id, bbox[0], bbox[1], bbox[2], bbox[3]])
     frame_idx = 0 
     # last_idx = dataset.size - 1
-    last_idx = 10
+    last_idx = 150
+    output_image_folder = \
+        os.path.splitext(output_file)[0]
+    first_idx, last_idx, image_shape, image_names = \
+        frame_idx,last_idx,dataset.image_shape[::-1],dataset.image_names
     visualizer = visualization.Visualization_only_save_image(
-        'output/3fps300s',frame_idx,last_idx,dataset.image_shape[::-1],dataset.image_names)
+        output_image_folder, first_idx, last_idx, image_shape, image_names)
     visualizer.run(frame_callback)
 
     
@@ -147,6 +160,9 @@ def run(sequence_dir, detection_file, output_file, min_confidence,
     for row in results:
         print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (
             row[0], row[1], row[2], row[3], row[4], row[5]),file=f)
+    if build_video:
+        build_video_cmd = f'ffmpeg -r 30 -i {os.path.join(output_image_folder,r"%04d.jpg")} {output_image_folder+".mp4"} -y'
+        os.system(build_video_cmd)
 
 
 def bool_string(input_string):
@@ -159,12 +175,6 @@ def parse_args():
     """ Parse command line arguments.
     """
     parser = argparse.ArgumentParser(description="Deep SORT")
-    parser.add_argument(
-        "--sequence_dir", help="Path to MOTChallenge sequence directory",
-        default=None, required=True)
-    parser.add_argument(
-        "--detection_file", help="Path to custom detections.", default=None,
-        required=True)
     parser.add_argument(
         "--output_file", help="Path to the tracking output file. This file will"
         " contain the tracking results on completion.",
@@ -189,12 +199,28 @@ def parse_args():
     parser.add_argument(
         "--display", help="Show intermediate tracking results",
         default=True, type=bool_string)
+    parser.add_argument(
+        "--draw_masks", help="Draw detections' masks.",
+        default=True, type=bool_string)
+    parser.add_argument(
+        "--draw_detections", help="Draw detections' bboxes.",
+        default=False, type=bool_string)
+    parser.add_argument(
+        "--draw_tracks", help="Draw track' bboxes.",
+        default=False, type=bool_string)
+    parser.add_argument(
+        "--draw_trails", help="Draw trails of same tracks.",
+        default=False, type=bool_string)
+    parser.add_argument(
+        "--build_video", help="Build a .mp4 file with outputed images.",
+        default=True, type=bool_string)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
     run(
-        args.sequence_dir, args.detection_file, args.output_file,
+        args.output_file, args.build_video,
         args.min_confidence, args.nms_max_overlap, args.min_detection_height,
-        args.max_cosine_distance, args.nn_budget, args.display)
+        args.max_cosine_distance, args.nn_budget, 
+        args.draw_masks, args.draw_detections, args.draw_tracks, args.draw_trails)
