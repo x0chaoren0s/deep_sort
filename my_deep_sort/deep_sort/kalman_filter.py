421f8fd0573333086da329f998e9f3938ee059d2
x0chaoren0s <x0chaoren0s@gmail.com>
2022-09-18 16:45:30 +0800

    ①原卡尔曼滤波器增加注释 ②新增kalmanFilter_QR

--- my_deep_sort/deep_sort/kalman_filter.py
+++ my_deep_sort/deep_sort/kalman_filter.py
@@ -153,7 +153,7 @@ class KalmanFilter(object):
 
         # 将均值向量映射到检测空间，即 Hx'
         mean = np.dot(self._update_mat, mean)
-        # 将协方差矩阵映射到检测空间，即 HP'H^T
+        # 将协方差矩阵映射到检测空间，即 HP'H^T+R
         covariance = np.linalg.multi_dot((
             self._update_mat, covariance, self._update_mat.T))
         return mean, covariance + innovation_cov
@@ -178,13 +178,18 @@ class KalmanFilter(object):
             Returns the measurement-corrected state distribution.
 
         """
-        # 将均值和协方差映射到检测空间，得到 Hx' 和 S
+        # 将均值和协方差映射到检测空间，得到 x=Hx' 和 S=HP'H^T+R
         projected_mean, projected_cov = self.project(mean, covariance)
 
-        # 矩阵分解
+        
+        # 计算卡尔曼增益 K=(P')(H.T)(S.-1)   S=HP'H.T+R
+        # 为避免求逆（运算量大且数值不稳定），两侧右乘S得KS=P'H.T ，转变成 XA=B 的形式
+        # 再将两边转置得 (S.T)(K.T)=(P'H.T).T ，获得 AX=B 的形式
+        # AX=B 可通过 scipy.linalg.cho_solve 快速求解，传参为 (A 的 Cholesky分解因子) 和 (B)
+        # 对A=S.T=S（协方差矩阵为对称阵）做Cholesky矩阵分解
         chol_factor, lower = scipy.linalg.cho_factor(
             projected_cov, lower=True, check_finite=False)
-        # 计算卡尔曼增益 K
+        # 传入A的Cholesky分解因子和B=(P'H.T).T，求解 AX=B   X=K.T   K=X.T
         kalman_gain = scipy.linalg.cho_solve(           # shape: (8, 4)
             (chol_factor, lower), np.dot(covariance, self._update_mat.T).T,
             check_finite=False).T
@@ -192,7 +197,8 @@ class KalmanFilter(object):
         innovation = measurement - projected_mean       # shape: (4,)
         # x = x' + Ky
         new_mean = mean + np.dot(innovation, kalman_gain.T) # mean.shape: (8,) np中 (8,4)*(4,)==(8,)==(4,)*(4,8)
-        # P = (I - KH)P'
+        # P = (I - KH)P'  但代码里是 P=P'-KSK.T
+        # 经https://zhuanlan.zhihu.com/p/497786053证明 KHP'==KSK.T
         new_covariance = covariance - np.linalg.multi_dot((
             kalman_gain, projected_cov, kalman_gain.T))
         return new_mean, new_covariance
@@ -200,6 +206,7 @@ class KalmanFilter(object):
     def gating_distance(self, mean, covariance, measurements,
                         only_position=False):
         """Compute gating distance between state distribution and measurements.
+        返回 mean(=x) 和 measurements(=z) 之间的马氏距离
 
         A suitable distance threshold can be obtained from `chi2inv95`. If
         `only_position` is False, the chi-square distribution has 4 degrees of
@@ -227,15 +234,121 @@ class KalmanFilter(object):
             `measurements[i]`.
 
         """
+        #将mean和convariance映射到检测空间
         mean, covariance = self.project(mean, covariance)
         if only_position:
             mean, covariance = mean[:2], covariance[:2, :2]
             measurements = measurements[:, :2]
 
+        d = measurements - mean # d=x-u
+        # 马氏距离公式定义：DM = sqrt(d * inv(cov) * d.T)  即  DM^2 = d * inv(cov) * d.T
+        # 但是按照定义直接计算 inv(cov) 过于笨重，因此下面使用 cholesky分解简化协方差求逆的运算
+        #   cholesky分解的前提是：
+        #   1.被分解矩阵需要是正定或半正定矩阵。（半正定分解的话，结果不唯一）
+        #   2.被分解矩阵需要是对称矩阵/Hermitian矩阵。（前者是实数域，后者是复数域）
+        #   协方差 cov 满足上述正定条件
+        # cov = L * L.T   (cholesky分解)
+        # 又cov是对称阵，故 L=L.T 也是对角阵
+        # 则 DM^2 = d * (L * L.T).-1 * d.T
+        #         = d * L.T.-1 * L.-1 * d.T
+        #         = (L.-1 * d.T).T  *  (L.-1 * d.T)
+        # 令 z = L.-1 * d.T
+        # 则 DM^2 = z.T * z
+        # 经验证，此解法能稳定跟踪，直接按定义写反而不能。。。。。
+        # 按定义写： squared_maha = np.sum(d @ np.linalg.inv(covariance) @ d.T, axis=0)
         cholesky_factor = np.linalg.cholesky(covariance)
-        d = measurements - mean
-        z = scipy.linalg.solve_triangular(
+        z = scipy.linalg.solve_triangular( # 求z = L.-1 * d.T 即解 L*z=d.T，因L是对角阵，故可用此函数快速求解
             cholesky_factor, d.T, lower=True, check_finite=False,
             overwrite_b=True)
         squared_maha = np.sum(z * z, axis=0)
-        return squared_maha
+        return squared_maha # shape: (22,)
+
+class kalmanFilter_QR(KalmanFilter):
+    """
+    定制卡尔曼滤波器的 Q(motion_cov:预测过程中噪声协方差) 和 R(innovation_cov:测量过程中噪声的协方差)
+    
+    Parameters in args
+    ----------
+    Q_times : float >0
+    R_times : float >0
+    """
+    def __init__(self, args):
+        '''
+        Parameters in args
+        ----------
+        Q_times : 超参数，用于调整 超参数motion_cov（过程噪声协方差） 的大小
+        R_times : 超参数，用于调整 超参数innovation_cov（观测噪声协方差） 的大小
+        '''
+        self.ndim, self.dt = 4, 1.
+
+        # Create Kalman filter model matrices.
+        self._motion_mat = np.eye(2 * self.ndim, 2 * self.ndim)
+        for i in range(self.ndim):
+            self._motion_mat[i, self.ndim + i] = self.dt
+        self._update_mat = np.eye(self.ndim, 2 * self.ndim)
+
+        # Motion and observation uncertainty are chosen relative to the current
+        # state estimate. These weights control the amount of uncertainty in
+        # the model. This is a bit hacky.
+        self._std_weight_position = 1. / 20
+        self._std_weight_velocity = 1. / 160
+        assert args.Q_times > 0 and args.R_times > 0
+        self.Q_times = args.Q_times
+        self.R_times = args.R_times
+
+    def predict(self, mean, covariance):
+        """Run Kalman filter prediction step.
+
+        Parameters
+        ----------
+        mean : ndarray
+            The 8 dimensional mean vector of the object state at the previous
+            time step.
+        covariance : ndarray
+            The 8x8 dimensional covariance matrix of the object state at the
+            previous time step.
+
+        Returns
+        -------
+        (ndarray, ndarray)
+            Returns the mean vector and covariance matrix of the predicted
+            state. Unobserved velocities are initialized to 0 mean.
+
+        """
+        # motion_cov == Q 预测过程中噪声协方差
+        motion_cov = np.diag(np.random.random(2*self.ndim)) * self.Q_times
+
+        # x' = Fx
+        mean = np.dot(self._motion_mat, mean)
+        # P' = FPF^T+Q
+        covariance = np.linalg.multi_dot((
+            self._motion_mat, covariance, self._motion_mat.T)) + motion_cov
+
+        return mean, covariance
+
+    def project(self, mean, covariance):
+        """Project state distribution to measurement space.
+
+        Parameters
+        ----------
+        mean : ndarray
+            The state's mean vector (8 dimensional array).
+        covariance : ndarray
+            The state's covariance matrix (8x8 dimensional).
+
+        Returns
+        -------
+        (ndarray, ndarray)
+            Returns the projected mean and covariance matrix of the given state
+            estimate.
+
+        """
+        # innovation_cov == R 测量过程中噪声的协方差
+        innovation_cov = np.diag(np.random.random(self.ndim)) * self.R_times
+
+        # 将均值向量映射到检测空间，即 Hx'
+        mean = np.dot(self._update_mat, mean)
+        # 将协方差矩阵映射到检测空间，即 HP'H^T+R
+        covariance = np.linalg.multi_dot((
+            self._update_mat, covariance, self._update_mat.T))
+        return mean, covariance + innovation_cov
