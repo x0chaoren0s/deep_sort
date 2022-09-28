c59531a56d8761c6e7822954fd8d6ff1ed096428
x0chaoren0s <x0chaoren0s@gmail.com>
2022-09-28 14:43:18 +0800

    ①x1,y1,x2,y2 -> x1,y1,w,h ②打印模型结构

--- my_deep_sort/utils/detector.py
+++ my_deep_sort/utils/detector.py
@@ -35,7 +35,24 @@ class Detector_mmdet(Detector_base):
         返回一个tuple：( [np.array(100个检测框：[x1,y1,w,h,confidence],[],..)],  [[100个mask：np.array(..),np.array()，..]]  )
         '''
         if isinstance(img, str) or isinstance(img, np.ndarray):
-            return inference_detector(self.model, img)
+            bbox_results, mask_results = inference_detector(self.model, img) # (bbox_results, mask_results) == ( [np.array(100个检测框：[x1,y1,x2,y2,confidence],[],..)],  [[100个mask：np.array(..),np.array()，..]]  )
+            bbox_results[0][:,2] -= bbox_results[0][:,0]  # x1,y1,x2,y2 -> x1,y1,w,h
+            bbox_results[0][:,3] -= bbox_results[0][:,1]
+            return bbox_results, mask_results   # ( [np.array(100个检测框：[x1,y1,w,h,confidence],[],..)],  [[100个mask：np.array(..),np.array()，..]]  )
         else:
             raise ValueError('not implemented.')
 
+    def summary(self):
+        '''
+        打印模型结构。
+        '''
+        print(self.model)
+
+
+config_file = "/home/xxy/mmdetection/configs/my_configs/mask2formerBiggestcontour_swin-s-p4-w7-224_lsj_8x2_50e_coco.py"
+# config_file = "/home/xxy/mmdetection/configs/my_configs/mask2former_swin-s-p4-w7-224_lsj_8x2_50e_coco.py"
+checkpoint_file = "/home/xxy/mmdetection/work_dirs/mask2former_swin-s-p4-w7-224_lsj_8x2_50e_coco_FlatCosineAnnealing/epoch_2000.pth"
+
+if __name__ == '__main__':
+    d = Detector_mmdet(config_file, checkpoint_file)
+    d.summary()
